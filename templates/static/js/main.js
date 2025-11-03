0c802c8d6be65c5f0423b8bf94558df85e5bcfc6
BarlowAILabs <tevin@barlowailabs.com>
2025-11-03 04:51:03 +0200

    feat: Connect contact form to Firebase Function and fix all JS

--- templates/static/js/main.js
+++ templates/static/js/main.js
@@ -1,3 +1,4 @@
+// Final version
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
